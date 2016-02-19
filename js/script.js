82b3b2f72a2caa745e8b39cb264e26fcb617a64a
razahaider <sayedhaiderr@gmail.com>
2016-02-20 01:04:57 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,160 @@
+$(document).one('pageinit', function(){
+
+	showRuns();
+
+	$('#submitAdd').on('tap', addRun);
+
+	
+	$('#submitEdit').on('tap', editRun);
+
+
+	$('#stats').on('tap','#deleteLink', deleteRun);
+
+	$('#stats').on('tap','#editLink', setCurrent);
+
+	
+	$('#clearRuns').on('tap', clearRuns);
+
+
+	function showRuns(){
+		var runs = getrunsObject();
+
+		if(runs != '' && runs != null){
+			for(var i = 0; i< runs.length; i++){
+				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>'+runs[i]["date"]+
+					'<br><strong>Distance:</strong>'+runs[i]["miles"]+'m<div class="controls">'+
+					'<a href="#edit" id="editLink" data-miles="'+runs[i]["miles"]+'"data-date="'+runs[i]["date"]+'">Edit</a> | <a href="#" id="deleteLink" data-miles="'+runs[i]["miles"]+'"data-date="'+runs[i]["date"]+'" onclick="return confirm(\'Are You Sure?\')">Delete</a></li>');
+			}
+			$('#home').bind('pageinit', function(){
+					$('#stats').listview('refresh');
+			});
+		} else{
+			
+		$('#stats').html('<p>No logs found</p>');
+
+		}
+	}
+
+	function addRun(){
+		var miles = $('#addMiles').val();
+		var date  = $('#addDate').val();
+
+
+		var run = {
+			date: date,
+			miles: parseFloat(miles)
+		};
+
+		var runs = getrunsObject();
+
+		runs.push(run);
+
+		alert("Record Added");
+
+		localStorage.setItem('runs', JSON.stringify(runs));
+
+		window.location.href="index.html";
+
+		return false;
+	}
+
+	function editRun(){
+
+		currentMiles = localStorage.getItem('currentMiles');
+		currentDate = localStorage.getItem('currentDate');
+
+		var runs = getrunsObject();
+
+
+		for(var i= 0; i< runs.length;i++){
+			if(runs[i].miles == currentMiles && runs[i].date == currentDate){
+				runs.splice(i, 1);
+
+			}
+			localStorage.setItem('runs', JSON.stringify(runs));
+		}
+
+		var miles = $('#editMiles').val();
+		var date  = $('#editDate').val();
+
+
+		var update_run = {
+			date: date,
+			miles: parseFloat(miles)
+		};
+
+		runs.push(update_run);
+
+		alert("Record Updated");
+
+		localStorage.setItem('runs', JSON.stringify(runs));
+
+		window.location.href="index.html";
+
+		return false;
+
+	}
+
+	function clearRuns(){
+		localStorage.removeItem('runs');
+		$('#stats').html('<p>No logs found</p>');
+	}
+
+
+	function deleteRun(){
+			localStorage.setItem('currentMiles', $(this).data('miles'));
+			localStorage.setItem('currentDate', $(this).data('date'));
+
+
+			currentMiles = localStorage.getItem('currentMiles');
+			currentDate = localStorage.getItem('currentDate');
+
+			var runs = getrunsObject();
+
+
+			for(var i= 0; i< runs.length;i++){
+				if(runs[i].miles == currentMiles && runs[i].date == currentDate){
+					runs.splice(i, 1);
+
+				}
+				localStorage.setItem('runs', JSON.stringify(runs));
+			}
+
+		
+			alert("Record Deleted");
+
+			
+			window.location.href="index.html";
+
+			return false;
+
+		}
+
+
+
+
+		function getrunsObject(){
+			var runs = new Array();
+			var currentRuns = localStorage.getItem('runs');
+
+			if(currentRuns != null){
+				var runs = JSON.parse(currentRuns);
+
+			}
+
+			return runs.sort(function(a, b){return new Date(b.date) - new Date(a.date)});
+		}
+
+
+		function setCurrent(){
+			localStorage.setItem('currentMiles', $(this).data('miles'));
+			localStorage.setItem('currentDate', $(this).data('date'));
+
+			$('#editMiles').val(localStorage.getItem('currentMiles'));
+			$('#editDate').val(localStorage.getItem('currentDate'));
+			}
+	
+	
+
+
+});
